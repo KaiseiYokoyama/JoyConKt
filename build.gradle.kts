bb5166a8dcb34cd7b88a5e6dbdd54b496f4dfff7
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-12-22 21:31:39 +0900

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "me.kyokoyama.joyconkt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.hid4java:hid4java:0.7.0")
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
